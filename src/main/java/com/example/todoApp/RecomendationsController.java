c95d79648de74116979ee39b3d2cdcd5fe29d5a9
LinaKK <lilikim0112@gmail.com>
2023-06-13 00:46:57 +0900

    merge test

--- src/main/java/com/example/todoApp/RecomendationsController.java
+++ src/main/java/com/example/todoApp/RecomendationsController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecomendationsController {
+
+    //"To-do Application !"출력
     @GetMapping("/")
     public String HelloWorld(){
         return "To-do Application !";
